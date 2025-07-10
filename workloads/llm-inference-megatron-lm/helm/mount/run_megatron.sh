d3dac629b92a97f8ce3ceacc8c48f859c7daa74b
eliecerecology <ediazdia@amd.com>
2025-07-10 18:01:15 +0300

    added the newest version on llm-inference-megatron-lm combining workload for inference on Llama 3.1 8B and 70B
    * added the newest version on llm-inference-megatron-lm combined
    
    * Update workloads/llm-inference-megatron-lm/helm/mount/run_megatron.sh
    
    * precommit fixed
    
    * Resolve the last comments in the code
    
    * resolving and testing tokenizer path
    
    * adding the kaiwo comments
    
    * precommits made
    
    ---------
    
    Co-authored-by: aivanni <4340981+aivanni@users.noreply.github.com>

--- workloads/llm-inference-megatron-lm/helm/mount/run_megatron.sh
+++ workloads/llm-inference-megatron-lm/helm/mount/run_megatron.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-
 # Set master address and port
 MASTER_ADDR=localhost
 MASTER_PORT=23731
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+export NCCL_DEBUG=INFO
+export NCCL_IB_DISABLE=1
+export NCCL_P2P_DISABLE=1
+export CUDA_LAUNCH_BLOCKING=1
 
-# Command to run the text generation server
 echo "Starting the text generation server with torchrun..."
 
-
-
 torchrun \
-    --nproc_per_node 1 \
-    --nnodes 1 \
-    --node_rank 0 \
+    --nproc_per_node $NPROC_PER_NODE \
+    --nnodes $NNODES \
+    --node_rank $NODE_RANK \
     --master_addr $MASTER_ADDR \
     --master_port $MASTER_PORT \
     /workspace/Megatron-LM/tools/run_text_generation_server.py \
@@ -22,10 +23,10 @@ torchrun \
     --no-masked-softmax-fusion \
     --no-rope-fusion \
     --no-gradient-accumulation-fusion \
-    --tensor-model-parallel-size 1 \
-    --pipeline-model-parallel-size 1 \
-    --context-parallel-size 1 \
+    --tensor-model-parallel-size $TENSOR_MODEL_PARALLEL_SIZE \
+    --pipeline-model-parallel-size $PIPELINE_MODEL_PARALLEL_SIZE \
+    --context-parallel-size $CONTEXT_PARALLEL_SIZE \
     --tokenizer-type HuggingFaceTokenizer \
     --tokenizer-model /workload/tokenizer/ \
     --load /workload/model/ \
-    --micro-batch-size 1
+    --micro-batch-size $MICRO_BATCH_SIZE
